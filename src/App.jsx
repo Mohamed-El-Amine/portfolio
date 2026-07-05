62072fd4a2afa7c51f1fda4f6294a29b51bc68f9
Mohamed El Amine <II.HAMOUDE.ii@GMAIL.com>
2026-07-05 18:32:29 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Tiles from './components/Tiles'
+import Tiles2 from './components/Tiles2'
 import Footer from './components/Footer'
 import Formation from './components/Formation'
 import Header from './components/Header'
@@ -10,6 +11,8 @@ import Work from './components/Work'
 
 import "./fonts/css/expose.css"
 import "./fonts/css/satoshi.css"
+import "./fonts/css/abuhmeda.css"
+import "./fonts/css/neckar.css"
 import './styles/index.css'
 
 function App() {
@@ -19,7 +22,7 @@ function App() {
       <Hero />
       <Tiles />
       <IconList />
-      <LogoBanner />
+      {/* <LogoBanner /> */}
       <Work />
       {/* <Skills /> */}
       <Formation />
